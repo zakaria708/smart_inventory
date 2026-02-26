13c39446bc8e90fdfb2e7eef42b0144479121318
Your Name <you@example.com>
2026-02-26 02:03:34 +0000

    v1

--- smart_site/boutique/views.py
+++ smart_site/boutique/views.py
@@ -204,6 +204,7 @@ def ajouter_produit(request):
         )
 
         messages.success(request, "Produit ajouté avec succès.")
+        return redirect("liste_produits")
 
     return render(request, "boutique/produit_form.html")
 
